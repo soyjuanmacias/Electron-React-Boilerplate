3844347cbbd779e43258ffd7238e7b818547fef8
Juan Macías <juan.macias@opinno.com>
2018-06-05 17:40:20 +0200

    Configuring react

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 import { Switch, Route } from 'react-router';
-import App from './containers/App';
+import App from './app';
 import HomePage from './containers/HomePage';
 import Counter from './components/Counter';
 
